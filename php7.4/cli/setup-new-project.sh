e4961f4a16e942d03c673c867031409a860b78ad
Suson Waiba <susonwaiba@gmail.com>
2020-10-03 22:03:22 +0545

    updated new project setup script

--- php7.4/cli/setup-new-project.sh
+++ php7.4/cli/setup-new-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "------------------------------------------"
-echo "Setting new wordpress project"
+echo "Installing new wordpress project"
 
 PROJECT_ROOT="/var/www/project"
 
@@ -9,7 +9,6 @@ cd ${PROJECT_ROOT}
 
 echo "- downloading latest version"
 curl -O https://wordpress.org/latest.tar.gz
-
 
 echo "- decompressing latest.tar.gz"
 tar -zxvf latest.tar.gz
@@ -21,18 +20,14 @@ cd ..
 
 echo "- setting wp-config.php"
 cp wp-config-sample.php wp-config.php
-perl -pi -e "s/database_name_here/project/g" wp-config.php
-perl -pi -e "s/username_here/root/g" wp-config.php
-perl -pi -e "s/password_here/root/g" wp-config.php
-perl -pi -e "s/localhost/mariadb/g" wp-config.php
-perl -i -pe'
-  BEGIN {
-    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-  }
-  s/put your unique phrase here/salt()/ge
-' wp-config.php
+wp --allow-root config set DB_NAME project
+wp --allow-root config set DB_USER root
+wp --allow-root config set DB_PASSWORD root
+wp --allow-root config set DB_HOST mariadb
+wp --allow-root config set WP_DEBUG 1
+wp --allow-root config shuffle-salts
+
+wp --allow-root config list
 
 echo "- cleaning up"
 rm -rf wordpress
