6a40e78d6d4a8c6e2e38ca48d70dceb635337086
Suson Waiba <susonwaiba@gmail.com>
2020-10-06 19:38:13 +0545

    support for variables and mailcatcher service added

--- php7.4/cli/docker-entrypoint.sh
+++ php7.4/cli/docker-entrypoint.sh
@@ -2,4 +2,9 @@
 
 /root/manage-project-permission.sh
 
+if [ ${PHP_ENABLE_XDEBUG} -eq "1" ]; then
+    docker-php-ext-enable xdebug
+    echo "-- php cli xdebug extension enabled"
+fi
+
 tail -f /dev/null
